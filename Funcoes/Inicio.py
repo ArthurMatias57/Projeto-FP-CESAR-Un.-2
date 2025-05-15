ff751c3822cf3ec18b0bc5d01904b0460b1830ab
Arthur Coelho Moraes Matias <arthur.trabalhoestudos@gmail.com>
2025-05-15 09:19:31 -0300

    Alterações feitas para união das funcionalidades na função inicio

--- Funcoes/Inicio.py
+++ Funcoes/Inicio.py
@@ -1,19 +1,18 @@
-import Funcoes
-import Funcoes.SugestoesPersonalizadas
-import Funcoes.cadastro_eventos
-import Funcoes.metas_saude
+from cadastro_eventos import *
+from metas_saude import *
+from CRUD import *
+from SugestoesPersonalizadas import *
 
 def inicio():
     print("Vida PET")
     selecao = int(input("Selecione uma das opções: \n1- CRUD\n2- Metas de saúde\n3- Sugestões personalizadas\n4- Cadastro de eventos"))
     match selecao:
         case 1:
-            Funcoes.CRUD.CRUD()
+            CRUD()
         case 2:
-            Funcoes.metas_saude.metasDeSaude()
+            metasDeSaude()
         case 3:
-            Funcoes.SugestoesPersonalizadas.sugestoesPersonalizadas()
+            sugestoesPersonalizadas()
         case 4:
-            Funcoes.cadastro_eventos.registrar_evento()
+            registrar_evento()
 inicio()
-print("BCt")
